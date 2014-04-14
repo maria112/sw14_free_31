17f7a628f163c34c4c9867e45f81ce2d17b81681
Maria Riffnaller-Schiefer <maria.riffnaller@gmail.com>
2014-04-14 19:23:01 +0200

    [MR,BB]{story} I01 - Display menus in main activity

--- TUGrazMenu/src/at/tugraz/tugrazmenu/MenuActivity.java
+++ TUGrazMenu/src/at/tugraz/tugrazmenu/MenuActivity.java
@@ -2,13 +2,11 @@ package at.tugraz.tugrazmenu;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Debug;
-import android.util.Log;
+import android.widget.ListView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
+
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuActivity extends Activity {
@@ -16,11 +14,27 @@ public class MenuActivity extends Activity {
     String urlString = "http://rss.tugraz.at/menue.xml";
     URL feedURL;
     List<MenuItem> menuItems;
+    MenuListAdapter menuAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+    }
+    
+    protected void onStart() {
+    	super.onStart();
+    	//TODO - Just for testing
+        menuItems = new ArrayList<MenuItem>();
+        menuItems.add(new MenuItem("Menü 1", "", "", "gesunde Mahlzeit", ""));
+        menuItems.add(new MenuItem("Menü 2", "", "", "ausgewogene Mahlzeit", ""));
+        loadMenus(menuItems);
+    }
+    
+    public void loadMenus(List<MenuItem> menus)
+    {
+        menuAdapter = new MenuListAdapter(this, menus);
+        ListView items = (ListView) findViewById(R.id.listMenuItems);
+        items.setAdapter(menuAdapter);
     }
 }
---
+++ TUGrazMenu/src/at/tugraz/tugrazmenu/MenuListAdapter.java
@@ -0,0 +1,50 @@
+package at.tugraz.tugrazmenu;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MenuListAdapter extends BaseAdapter {
+	private Context context;
+	private List<MenuItem> list;
+	
+	public MenuListAdapter(Context context, List<MenuItem> list) {
+		this.context = context; 
+		this.list = list;
+	}
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position; 
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if(convertView == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.fragment_menuitem, null);
+		}
+		TextView txtTitle = (TextView) convertView.findViewById(R.id.textTitle); 
+		TextView txtDescription = (TextView) convertView.findViewById(R.id.textDescription);
+		txtTitle.setText(list.get(position).title);
+		txtDescription.setText(list.get(position).description);
+		
+		return convertView;
+	}
+}
