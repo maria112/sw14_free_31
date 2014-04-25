07fd1be37d14829dda01a2ef1d9d2a735e1ffcab
Maria Riffnaller-Schiefer <maria.riffnaller@gmail.com>
2014-04-25 14:19:00 +0200

    [MR,BB]{test before code} I01 - Add tests for DataStore class

---
+++ TUGrazMenuTest/src/at/tugraz/tugrazmenu/DataStoreTest.java
@@ -0,0 +1,34 @@
+package at.tugraz.tugrazmenu;
+
+import junit.framework.TestCase;
+
+public class DataStoreTest extends TestCase {
+	
+	class NotificationHandler implements DataStore.DataStoreNotification{
+		
+		boolean haveData = false; 
+			
+		public void onDataLoaded() {
+			haveData = true; 
+		}
+		
+		public boolean getHaveData() {
+			return haveData;
+		}
+	}
+	
+	public void testLoadData(){
+		NotificationHandler handler = new NotificationHandler();
+		DataStore.loadData(handler);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, handler.getHaveData());
+		
+		assertEquals(false,  DataStore.getRestaurants().isEmpty());
+		assertEquals(false,  DataStore.getRestaurantMenus().isEmpty());
+	}
+
+}
