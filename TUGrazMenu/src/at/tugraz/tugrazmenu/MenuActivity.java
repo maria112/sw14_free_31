9a59f50e4eebf478a66be53f73df471e776158f7
Maria Riffnaller-Schiefer <maria.riffnaller@gmail.com>
2014-04-27 19:47:00 +0200

    [MR,BB]{story} I01 - Display only todays menus

--- TUGrazMenu/src/at/tugraz/tugrazmenu/MenuActivity.java
+++ TUGrazMenu/src/at/tugraz/tugrazmenu/MenuActivity.java
@@ -2,8 +2,12 @@ package at.tugraz.tugrazmenu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 
@@ -28,9 +32,36 @@ public class MenuActivity extends Activity implements DataStore.DataStoreNotific
 		MenuActivity.this.runOnUiThread(new Runnable() {
 	        @Override
 	        public void run() {
-	        	loadMenus(DataStore.getRestaurants(), DataStore.getRestaurantMenus());
+	        	List<Restaurant> currentRestaurants = new ArrayList<Restaurant>();
+	            List<List<MenuItem>> currentRestaurantMenus = new ArrayList<List<MenuItem>>();
+	            filterMenus(DataStore.getRestaurants(), DataStore.getRestaurantMenus(),
+	            		currentRestaurants, currentRestaurantMenus);
+	        	loadMenus(currentRestaurants, currentRestaurantMenus);
 	        }
 		});
+    }
+    
+    public void filterMenus(List<Restaurant> allRestaurants, List<List<MenuItem>> allRestaurantMenus,
+    		List<Restaurant> currentRestaurants, List<List<MenuItem>> currentRestaurantMenus) {
+    	Calendar today = new GregorianCalendar();
+        today.add(Calendar.DAY_OF_MONTH, -2);
+        
+        for(int restaurantIndex = 0; restaurantIndex < allRestaurants.size(); restaurantIndex++){
+        	List<MenuItem> currentMenus = new ArrayList<MenuItem>();
+        	
+        	for(int menuIndex = 0; menuIndex < allRestaurantMenus.get(restaurantIndex).size(); menuIndex++) {
+        		MenuItem currentMenu = allRestaurantMenus.get(restaurantIndex).get(menuIndex);
+        		if(currentMenu.date.get(Calendar.ERA) == today.get(Calendar.ERA) &&
+        			currentMenu.date.get(Calendar.YEAR) == today.get(Calendar.YEAR) &&
+        			currentMenu.date.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR)) {
+        			currentMenus.add(currentMenu);
+        		}
+        	}
+        	if(!currentMenus.isEmpty()){
+        		currentRestaurantMenus.add(currentMenus);
+        		currentRestaurants.add(allRestaurants.get(restaurantIndex));
+        	}
+        }  
     }
     
     public void loadMenus(List<Restaurant> restaurants, List<List<MenuItem>> menus) {
