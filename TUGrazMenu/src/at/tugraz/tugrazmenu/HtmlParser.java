f153eddb1070788faff9fdbeae39872b8dc86974
Benny Bristow <benny6890@gmail.com>
2014-05-18 15:23:24 +0200

    [BB,MR]{test before Code} - I04 - Tests for getting future MenuItems from html page added

---
+++ TUGrazMenu/src/at/tugraz/tugrazmenu/HtmlParser.java
@@ -0,0 +1,26 @@
+package at.tugraz.tugrazmenu;
+
+import java.util.*;
+
+public class HtmlParser extends XmlHelper {
+
+    public String getDateString(String formattedHtmlContent) {
+        return null;
+    }
+
+    public GregorianCalendar stringToGregorianCalendar(String dateString){
+        return null;
+    }
+
+    public List<GregorianCalendar> getWeekDates(GregorianCalendar calendar) {
+        return null;
+    }
+
+    public void populateItemAndRestaurantLists(GregorianCalendar startDate, String htmlContent, List<Restaurant> restaurants, List<MenuItem> menuItems){
+    }
+
+    public int menuItemExistsInListAt(List<MenuItem> menuItemList, MenuItem currentItem) {
+    return 0;
+    }
+  }
+
